950db8a693637f040bb8ef1508b1f39bbbe5d93b
juan benega <jmbenega@gmail.com>
2022-11-16 06:40:17 -0300

    agregue proyectos, corregí el scroll del background

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,10 +3,14 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Footer from "./Footer"
 import Header from "./Header"
-import { useContext } from 'react';
-import { ModeContext } from "../context/ModeContext";
+import { useContext } from 'react'
+import { ModeContext } from "../context/ModeContext"
 import cya from '../img/cuerpoyarmonia.PNG'
 import palomo from '../img/palomochat.PNG'
+import mascotas from '../img/mascotas.PNG'
+import cyaecom from '../img/cuerpoyarmoniaecommerce.PNG'
+
+
 
 const Projects = () => {
 
@@ -21,17 +25,24 @@ const Projects = () => {
                         <h1 className="projectTitle">Individuales</h1>
                     </Col>
                     <Col>
-                        <h1>Ecommerce</h1>
+                        <h1>Sitio web sobre bienestar y salud</h1>
                         <img className="projectImg" src={cya} alt="Ecommerce Cuerpo y Armonía" />
+                        <div className="projectLinkGroup">
+                            <a href="https://github.com/JuanBenega/Proyecto-Desarrollo-Web" className="projectLink" target={'_blank'}>
+                                <h3 className="subtitle">Github</h3></a>
+                            <a href="https://juanbenega.github.io/Proyecto-Desarrollo-Web/" className="projectLink" target={'_blank'}>
+                                <h3 className="subtitle">Deploy</h3></a>
+                        </div>
+                    </Col>
+                    <Col>
+                    <h1>Ecommerce</h1>
+                        <img className="projectImg" src={cyaecom} alt="Ecommerce Cuerpo y Armonía" />
                         <div className="projectLinkGroup">
                             <a href="https://github.com/JuanBenega/proyecto-react" className="projectLink" target={'_blank'}>
                                 <h3 className="subtitle">Github</h3></a>
                             <a href="https://proyecto-react-ashen.vercel.app/" className="projectLink" target={'_blank'}>
                                 <h3 className="subtitle">Deploy</h3></a>
                         </div>
-                    </Col>
-                    <Col>
-                        <img className="projectImg" src={cya} alt="Ecommerce Cuerpo y Armonía" />
                     </Col>
                 </Row>
                 <Row className="projectGroup">
@@ -49,7 +60,14 @@ const Projects = () => {
                         </div>
                     </Col>
                     <Col>
-                        <img className="projectImg" src={cya} alt="Ecommerce Cuerpo y Armonía" />
+                    <h1>Portal para mascotas perdidas</h1>
+                        <img className="projectImg" src={mascotas} alt="Portal para mascotas perdidas" />
+                        <div className="projectLinkGroup">
+                            <a href="https://github.com/No-Country/c6-32-m-jquery-node" className="projectLink" target={'_blank'}>
+                                <h3 className="subtitle">Github</h3></a>
+                            <a href="https://pet-search.netlify.app/home" className="projectLink" target={'_blank'}>
+                                <h3 className="subtitle">Deploy</h3></a>
+                        </div>
                     </Col>
                 </Row>
             </Container>
