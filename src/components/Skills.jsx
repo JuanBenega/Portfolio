82784e94978c033020ce1939a296cc6bc489b6c4
Juan Benega <104471152+JuanBenega@users.noreply.github.com>
2022-11-24 18:51:09 -0300

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -47,7 +47,7 @@ const Skills = () => {
                     </a>
                     <a target={'_blank'} href="https://vercel.com/" className="tooltip">
                         <img className="icon thumbails" src="https://camo.githubusercontent.com/add2c9721e333f0043ac938f3dadbc26a282776e01b95b308fcaba5afaf74ae3/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313538383830353835382f7265706f7369746f726965732f76657263656c2f6c6f676f2e706e67" alt="Vercel" />
-                        <span className="tooltiptext">Vervel</span>
+                        <span className="tooltiptext">Vercel</span>
                     </a>
                     <a target={'_blank'} href="https://www.postman.com/" className="tooltip">
                         <img className="icon thumbails" src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/512/external-postman-is-the-only-complete-api-development-environment-logo-color-tal-revivo.png" alt="Postman" />
